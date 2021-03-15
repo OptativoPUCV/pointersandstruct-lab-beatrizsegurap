95feaabfbe1c381d80654889376b5e38a86940a3
Replit user <replituser@example.com>
2021-03-15 15:00:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -58,6 +58,10 @@ typedef struct {
 
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
+  Persona p;
+  strcpy(p.nombre,nombre);
+  strcpy(p.rut,rut);
+  p.edad=edad;
    return NULL;
 }
 
